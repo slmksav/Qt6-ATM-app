d2a1da3cf9839fd018d205c66510600f761d5b9e
Sauli <sauli.savinainen@gmail.com>
2023-04-01 17:59:34 +0300

    korjattu DLLPinCode että sen voi compileta

--- frontend/lib/DLLPinCode/dllpincode.cpp
+++ frontend/lib/DLLPinCode/dllpincode.cpp
@@ -1,5 +1,1 @@
-#include "dllpincode.h"
 
-DLLPinCode::DLLPinCode()
-{
-}
--- frontend/lib/DLLPinCode/pincodeui.cpp
+++ frontend/lib/DLLPinCode/pincodeui.cpp
@@ -1,14 +1,1 @@
 #include "pincodeui.h"
-#include "ui_pincodeui.h"
-
-PinCodeUi::PinCodeUi(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::PinCodeUi)
-{
-    ui->setupUi(this);
-}
-
-PinCodeUi::~PinCodeUi()
-{
-    delete ui;
-}
