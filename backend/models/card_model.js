9b40773ac91e3cb236b9dc6811c7054791dfe062
Sauli <sauli.savinainen@gmail.com>
2023-04-11 11:34:14 +0300

    getCardIdByHexCode
    + REST API:ssa ja PinCode dllssä

--- backend/models/card_model.js
+++ backend/models/card_model.js
@@ -4,15 +4,29 @@ const card = {
   getAll: function(callback) {
     return db.query('select * from card', callback);
   },
+
   getById: function(id, callback) {
     return db.query('select * from card where idcard=?', [id], callback);
   },
-  getById: function(id, callback) {
-    return db.query('select * from card where idcard=?', [id], callback);
+
+  getCardIdByHexCode: function(cardhexcode) {
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT idcard FROM card WHERE cardhexcode = ?';
+      db.query(query, [cardhexcode], (err, results) => {
+        if (err) reject(err);
+        if (results.length > 0) {
+          resolve(results[0].idcard);
+        } else {
+          reject(new Error('Card not found'));
+        }
+      });
+    });
   },
+
   delete: function(id, callback) {
     return db.query('delete from card where idcard=?', [id], callback);
   },
+
   update: function(id, card, callback) {
     return db.query(
       'update card set wrongAttempts=? where idcard=?',
