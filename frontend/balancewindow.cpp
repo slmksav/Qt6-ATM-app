953ce0c192216f23537d01bac85dca88374dd154
Miika Tiihonen <m95.tiihonen@gmail.com>
2023-04-14 14:58:42 +0300

    balancewin language

--- frontend/balancewindow.cpp
+++ frontend/balancewindow.cpp
@@ -26,6 +26,7 @@ void BalanceWindow::putSessionData(SessionData *session)
 
 void BalanceWindow::updateUI()
 {
+    //debit and credit values
     if(session->accountType == "dual")
     {
         ui->labelBalance->setText(QString::number(session->accountBalance, 'f', 2));
@@ -40,6 +41,24 @@ void BalanceWindow::updateUI()
     {
         ui->labelBalance->setText("-");
         ui->labelCredit->setText(QString::number(session->accountCredit, 'f', 2));
+    }
+
+    //other ui elements
+    if(session->language == "fi")
+    {
+        ui->buttonLogout->setText("Kirjaudu Ulos");
+        ui->buttonReturn->setText("Palaa");
+
+        ui->labelInfoBalance->setText("Saldo:");
+        ui->labelInfoCredit->setText("Luotto:");
+    }
+    if(session->language == "en")
+    {
+        ui->buttonLogout->setText("Logout");
+        ui->buttonReturn->setText("Return");
+
+        ui->labelInfoBalance->setText("Balance:");
+        ui->labelInfoCredit->setText("Credit:");
     }
 }
 
