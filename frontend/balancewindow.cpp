c9fc9728697812d2e0109671c2ab40807eb0e263
Miika Tiihonen <m95.tiihonen@gmail.com>
2023-04-16 11:45:53 +0300

    balancewin display withdrawable sums

--- frontend/balancewindow.cpp
+++ frontend/balancewindow.cpp
@@ -23,21 +23,61 @@ BalanceWindow::~BalanceWindow()
 
 void BalanceWindow::updateUI()
 {
+    //quick maths
+    //decimate withdrawable sums
+    double balanceAvailable = 0.00;
+    double creditAvailable = 0.00;
+    if(session->accountType == "debit" || session->accountType == "dual")
+    {
+        balanceAvailable = session->accountBalance;
+        qDebug() << Q_FUNC_INFO << "(int)(session->accountBalance * 100) % 1000 = "
+                 << (int)(balanceAvailable * 100) % 1000;
+
+        double cutoff = (int)(balanceAvailable * 100) % 1000;
+        balanceAvailable -= cutoff / 100.0;
+
+        qDebug() << Q_FUNC_INFO << "balanceAvailable: " << balanceAvailable;
+    }
+    if(session->accountType == "credit" || session->accountType == "dual")
+    {
+        creditAvailable = session->accountCreditMax - session->accountCredit;
+        qDebug() << Q_FUNC_INFO << "(int)(creditAvailable * 100) % 1000 = "
+                 << (int)(creditAvailable * 100) % 1000;
+
+        double cutoff = (int)(creditAvailable * 100) % 1000;
+        creditAvailable -= cutoff / 100.0;
+
+        qDebug() << Q_FUNC_INFO << "creditAvailable: " << creditAvailable;
+    }
+
+    //actual ui stuff below
     //debit and credit values
     if(session->accountType == "dual")
     {
         ui->labelBalance->setText(QString::number(session->accountBalance, 'f', 2));
         ui->labelCredit->setText(QString::number(session->accountCredit, 'f', 2));
+        ui->labelCreditMax->setText(QString::number(session->accountCreditMax, 'f', 2));
+
+        ui->labelBalanceAvail->setText(QString::number(balanceAvailable, 'f', 2));
+        ui->labelCreditAvail->setText(QString::number(creditAvailable, 'f', 2));
     }
     else if(session->accountType == "debit")
     {
         ui->labelBalance->setText(QString::number(session->accountBalance, 'f', 2));
         ui->labelCredit->setText("-");
+        ui->labelCreditMax->setText("-");
+
+        ui->labelBalanceAvail->setText(QString::number(balanceAvailable, 'f', 2));
+        ui->labelCreditAvail->setText("-");
     }
     else
     {
         ui->labelBalance->setText("-");
         ui->labelCredit->setText(QString::number(session->accountCredit, 'f', 2));
+        ui->labelCreditMax->setText(QString::number(session->accountCreditMax, 'f', 2));
+
+        ui->labelBalanceAvail->setText("-");
+        ui->labelCreditAvail->setText(QString::number(creditAvailable, 'f', 2));
     }
 
     //other ui elements
@@ -47,7 +87,11 @@ void BalanceWindow::updateUI()
         ui->buttonReturn->setText("Palaa");
 
         ui->labelInfoBalance->setText("Saldo:");
-        ui->labelInfoCredit->setText("Luotto:");
+        ui->labelInfoCredit->setText("Käytetty Luotto:");
+        ui->labelInfoCreditMax->setText("Luottokatto:");
+
+        ui->labelInfoBalanceAvail->setText("Nostettavissa:");
+        ui->labelInfoCreditAvail->setText("Nostettavissa:");
     }
     if(session->language == "en")
     {
@@ -55,7 +99,11 @@ void BalanceWindow::updateUI()
         ui->buttonReturn->setText("Return");
 
         ui->labelInfoBalance->setText("Balance:");
-        ui->labelInfoCredit->setText("Credit:");
+        ui->labelInfoCredit->setText("Used Credit:");
+        ui->labelInfoCreditMax->setText("Credit Cap:");
+
+        ui->labelInfoBalanceAvail->setText("Withdrawable:");
+        ui->labelInfoCreditAvail->setText("Withdrawable:");
     }
 }
 
